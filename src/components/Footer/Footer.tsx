2cf790d2eb924ca103722323b3215b5ebb196af2
Ameer <ak2260650@gmail.com>
2022-07-24 15:03:54 +0530

    removed timer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,10 +15,10 @@ const Footer: FC<FooterProps> = ({ moves, setTime }) => {
 
   return (
     <main className="flex items-center gap-7 w-full max-w-[32rem]">
-      <div className="flex-1 flex items-center justify-between bg-customGrey2/40 px-4 py-5 rounded-lg">
+      {/* <div className="flex-1 flex items-center justify-between bg-customGrey2/40 px-4 py-5 rounded-lg">
         <div className="text-customBlue2">Time</div>
         <div className="text-secondary font-bold">09:51</div>
-      </div>
+      </div> */}
       <div className="flex-1 px-4 py-5 flex items-center justify-between bg-primary text-neutral2  rounded-lg pointer">
         <div>Moves</div>
         <div className="font-bold">{String(moves).padStart(2, "0")}</div>
